a4597e695bfd7a9b0cb08d8608c3e9fab48de32c
Half_nothing <Half_nothing@163.com>
2024-04-11 17:03:04 +0800

    docs(HISTORY): 更新HISTORY文件

--- src/bot/app.py
+++ src/bot/app.py
@@ -1,11 +1,14 @@
 from os import getcwd
 from os.path import join
+from threading import Thread
+from time import sleep
 
 from satori import LoginStatus, WebsocketsInfo
 from satori.client import Account, App
 
 from src.base.config import config
 from src.base.logger import logger
+from src.bot.mcsm import mcsm
 from src.bot.message import MessageSender
 from src.element.permissions import Root
 from src.module.reply_message import ReplyManager
@@ -25,6 +28,16 @@ if not check_file(permission_image_dir):
     text_to_image(Root.instance, permission_image_dir)
 
 
+def update_mcsm_info_thread_handler():
+    while True:
+        logger.debug("Update MCSM info")
+        mcsm.update_instance_status()
+        sleep(config.config.mcsm_config.update_time)
+
+
+update_mcsm_info_thread = Thread(target=update_mcsm_info_thread_handler, daemon=True)
+
+
 @app.lifecycle
 async def on_startup(account: Account, status: LoginStatus):
     if account.self_id == config.config.login_config.qq:
@@ -32,6 +45,7 @@ async def on_startup(account: Account, status: LoginStatus):
             case LoginStatus.CONNECT:
                 logger.info(f"{account.self_id} Online")
                 message_sender.set_account(account)
+                update_mcsm_info_thread.start()
                 # await account.send_message(config.config.group_config.admin_group, f"Plugin online")
             case LoginStatus.OFFLINE:
                 logger.info(f"{account.self_id} Offline")

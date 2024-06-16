64d2efbf452fbe899a3ccd28c7fb950b7aa2fcf0
Half_nothing <Half_nothing@163.com>
2024-06-16 21:13:13 +0800

    fix(database): 修复数据库权限错误

--- src/bot/database.py
+++ src/bot/database.py
@@ -1,27 +1,32 @@
+from pymysql import OperationalError
+
+from src.base.logger import logger
 from src.base.config import config
 from src.module.database import Database
 
 database = Database(config.config.database)
 
-database.run_command("""CREATE TABLE IF NOT EXISTS `message`  (
-                      `id` int NOT NULL AUTO_INCREMENT,
-                      `message_id` char(128) NOT NULL,
-                      `sender_id` char(16) NOT NULL,
-                      `sender_name` varchar(255) NOT NULL,
-                      `group_id` char(16) NOT NULL,
-                      `group_name` varchar(255) NOT NULL,
-                      `is_command` tinyint(1) NOT NULL DEFAULT 0,
-                      `message` text NULL,
-                      `send_time` datetime NOT NULL,
-                      PRIMARY KEY (`id`, `message_id`),
-                      UNIQUE INDEX `index`(`id`, `message_id`) USING BTREE,
-                      INDEX `sender`(`sender_id`, `sender_name`) USING HASH,
-                      INDEX `group`(`group_id`, `group_name`) USING HASH);""")
-
-database.run_command("""set global event_scheduler = on;""")
-
-database.run_command("""CREATE EVENT IF NOT EXISTS `delete_expired_messages` 
+try:
+    database.run_command("""CREATE TABLE IF NOT EXISTS `message`  (
+                          `id` int NOT NULL AUTO_INCREMENT,
+                          `message_id` char(128) NOT NULL,
+                          `sender_id` char(16) NOT NULL,
+                          `sender_name` varchar(255) NOT NULL,
+                          `group_id` char(16) NOT NULL,
+                          `group_name` varchar(255) NOT NULL,
+                          `is_command` tinyint(1) NOT NULL DEFAULT 0,
+                          `message` text NULL,
+                          `send_time` datetime NOT NULL,
+                          PRIMARY KEY (`id`, `message_id`),
+                          UNIQUE INDEX `index`(`id`, `message_id`) USING BTREE,
+                          INDEX `sender`(`sender_id`, `sender_name`) USING HASH,
+                          INDEX `group`(`group_id`, `group_name`) USING HASH);""")
+    database.run_command("""set global event_scheduler = on;""")
+    database.run_command("""CREATE EVENT IF NOT EXISTS `delete_expired_messages` 
                         ON SCHEDULE
                         EVERY '1' DAY
                         DO DELETE FROM `message` 
                         WHERE TIMESTAMPDIFF(DAY, send_time, NOW()) > 15;""")
+except OperationalError as e:
+    logger.error(e)
+    logger.error(f"Database permission error")

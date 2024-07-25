775e6e37cdcbc634d9f5173cbf3ae85971896b95
Half_nothing <Half_nothing@163.com>
2024-07-25 19:35:37 +0800

    refactor(database): 修复权限不足报错问题,优化调试文本

--- src/bot/database.py
+++ src/bot/database.py
@@ -3,10 +3,12 @@ from pymysql import OperationalError
 from src.base.logger import logger
 from src.base.config import config
 from src.module.database import Database
-
-database = Database(config.config.database)
+from src.type.types import ReturnType
 
 try:
+    logger.debug("Database is initializing...")
+    database = Database(config.config.database)
+    logger.debug("Database connection established.")
     database.run_command("""CREATE TABLE IF NOT EXISTS `message`  (
                           `id` int NOT NULL AUTO_INCREMENT,
                           `message_id` char(128) NOT NULL,
@@ -21,12 +23,26 @@ try:
                           UNIQUE INDEX `index`(`id`, `message_id`) USING BTREE,
                           INDEX `sender`(`sender_id`, `sender_name`) USING HASH,
                           INDEX `group`(`group_id`, `group_name`) USING HASH);""")
-    database.run_command("""set global event_scheduler = on;""")
+    logger.trace("Checking event scheduler...")
+    res = database.run_command("""SHOW VARIABLES LIKE 'event_scheduler';""", [], ReturnType.ONE)
+    if res[1] == "OFF":
+        logger.warning("Event scheduler disable, try to enable it.")
+        try:
+            database.run_command("""set global event_scheduler = on;""")
+            logger.success("Event scheduler enabled.")
+        except OperationalError as e:
+            logger.error(e)
+            logger.error(f"Unable to set global event scheduler on database, please enable event scheduler")
+            exit(1)
+    logger.trace("Create event scheduler...")
     database.run_command("""CREATE EVENT IF NOT EXISTS `delete_expired_messages` 
                         ON SCHEDULE
                         EVERY '1' DAY
                         DO DELETE FROM `message` 
                         WHERE TIMESTAMPDIFF(DAY, send_time, NOW()) > 15;""")
-except OperationalError as e:
+    logger.trace("Event scheduler created.")
+    logger.success("Database initialized")
+except Exception as e:
     logger.error(e)
-    logger.error(f"Database permission error")
+    logger.error(f"Database init error")
+    exit(1)

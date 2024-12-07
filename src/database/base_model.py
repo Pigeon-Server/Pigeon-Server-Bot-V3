1cfdc84bbaad884caad292c6276caddc89c2dd51
Half_nothing <Half_nothing@163.com>
2024-12-07 09:51:44 +0800

    fix: 修复数据库链接断开后无法重连

--- src/database/base_model.py
+++ src/database/base_model.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 
-from peewee import Database, Model, MySQLDatabase, PostgresqlDatabase, SqliteDatabase
+from peewee import Database, Model
+from playhouse.pool import PooledMySQLDatabase, PooledPostgresqlDatabase, PooledSqliteDatabase
 
 from src.base.config import main_config
 from src.base.logger import logger
@@ -10,15 +11,25 @@ database: Database
 
 match main_config.database.type.lower():
     case 'mysql':
-        database = MySQLDatabase(database=main_config.database.database_name, host=main_config.database.host,
-                                 port=main_config.database.port, user=main_config.database.username,
-                                 password=main_config.database.password)
+        database = PooledMySQLDatabase(database=main_config.database.database_name,
+                                       host=main_config.database.host,
+                                       port=main_config.database.port,
+                                       user=main_config.database.username,
+                                       password=main_config.database.password,
+                                       max_connections=64,
+                                       stale_timeout=300)
     case 'sqlite':
-        database = SqliteDatabase(database=main_config.database.database_name)
+        database = PooledSqliteDatabase(database=main_config.database.database_name,
+                                        max_connections=64,
+                                        stale_timeout=300)
     case 'postgresql':
-        database = PostgresqlDatabase(database=main_config.database.database_name, host=main_config.database.host,
-                                      port=main_config.database.port, user=main_config.database.username,
-                                      password=main_config.database.password)
+        database = PooledPostgresqlDatabase(database=main_config.database.database_name,
+                                            host=main_config.database.host,
+                                            port=main_config.database.port,
+                                            user=main_config.database.username,
+                                            password=main_config.database.password,
+                                            max_connections=64,
+                                            stale_timeout=300)
     case _:
         logger.critical(f'Database type {main_config.database.type} not supported')
 
